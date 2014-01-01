0409e0794676a8367413a0fb487aad610919cad1
Brandon Logan <johnbrandonlogan@gmail.com>
2014-01-01 17:24:02 -0600

    First setup of files for multiple monitors extension.

---
+++ README.md
@@ -0,0 +1,6 @@
+CinnamonExtensions
+
+Collection of extensions for the cinnamon desktop manager.
+
+1. Multiple monitor panels.
+Add new panels to multiple monitors.
---
+++ multimonitorpanels@brandonlogan/extension.js
@@ -0,0 +1,15 @@
+const Main = imports.ui.main;
+const AppletManager = imports.ui.appletManager;
+const Lang = imports.lang;
+
+function enabled(){
+
+}
+
+function disable(){
+
+}
+
+function init(){
+
+}
